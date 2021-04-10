7ee50899527b2585f5fe3ecb54a9ab6d67e3dec6
Bill Peregoy <bill@peregoy.org>
2021-04-10 16:36:57 -0400

    Added tick function

--- lib/trs80_emulator/z80/z80.ex
+++ lib/trs80_emulator/z80/z80.ex
@@ -15,4 +15,16 @@ defmodule Trs80Emulator.Z80 do
   def reset(%State{} = state) do
     %{state | pc: <<0::size(16)>>, interrupt_reg: <<0::size(08)>>, refresh_reg: <<0::size(8)>>}
   end
+
+  def tick(%State{pc: <<high, low>>} = state) do
+    new_low = low + 1
+
+    new_high =
+      case low do
+        0xFF -> high + 1
+        _ -> high
+      end
+
+    %{state | pc: <<new_high, new_low>>}
+  end
 end
--- test/trs80_emulator/state_test.exs
+++ test/trs80_emulator/state_test.exs
@@ -23,4 +23,27 @@ defmodule Trs80Emulator.StateTest do
       assert new_state.refresh_reg == <<0::size(8)>>
     end
   end
+
+  describe "tick" do
+    test "increments PC" do
+      state = %State{pc: <<0::size(16)>>}
+
+      new_state = Z80.tick(state)
+      assert(new_state.pc == <<1::size(16)>>)
+    end
+
+    test "wraps on 8 bit PC boundary" do
+      state = %State{pc: <<0x00FF::size(16)>>}
+
+      new_state = Z80.tick(state)
+      assert(new_state.pc == <<0x0100::size(16)>>)
+    end
+
+    test "PC wraps at 16 bit boundary" do
+      state = %State{pc: <<0xFFFF::size(16)>>}
+
+      new_state = Z80.tick(state)
+      assert(new_state.pc == <<0::size(16)>>)
+    end
+  end
 end
