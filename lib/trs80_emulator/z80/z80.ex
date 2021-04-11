d4df8527b48dda4f7a44303249c60e21e6127155
Bill Peregoy <bill@peregoy.org>
2021-04-11 00:26:50 -0400

    Added all cases for ld

--- lib/trs80_emulator/z80/z80.ex
+++ lib/trs80_emulator/z80/z80.ex
@@ -56,8 +56,48 @@ defmodule Trs80Emulator.Z80 do
     |> execute_instruction()
   end
 
-  def execute_instruction(%{registers: registers} = z80) do
-    updated_registers = %{registers | a: <<27>>}
+  def execute_instruction(%{registers: registers, ir: ir} = z80) do
+    case ir do
+      <<0b01::size(2), dest_reg::size(3), src_reg::size(3)>> ->
+        ld(z80, src_reg, dest_reg)
+        IO.inspect(ir, label: "IR")
+        updated_registers = %{registers | a: <<27>>}
+        %{z80 | registers: updated_registers}
+
+      _ ->
+        z80
+    end
+  end
+
+  def ld(%{registers: registers} = z80, src_reg, dest_reg) do
+    source_value = get_register_value(registers, src_reg)
+    updated_registers = set_register_value(registers, dest_reg, source_value)
     %{z80 | registers: updated_registers}
+  end
+
+  def get_register_value(registers, src_reg) do
+    case src_reg do
+      <<0b000::size(3)>> -> registers.b
+      <<0b001::size(3)>> -> registers.c
+      <<0b010::size(3)>> -> registers.d
+      <<0b011::size(3)>> -> registers.e
+      <<0b100::size(3)>> -> registers.h
+      <<0b101::size(3)>> -> registers.l
+      <<0b111::size(3)>> -> registers.a
+      _ -> <<0b000>>
+    end
+  end
+
+  def set_register_value(registers, dest_reg, value) do
+    case dest_reg do
+      <<0b000::size(3)>> -> %{registers | b: value}
+      <<0b001::size(3)>> -> %{registers | c: value}
+      <<0b010::size(3)>> -> %{registers | d: value}
+      <<0b011::size(3)>> -> %{registers | e: value}
+      <<0b100::size(3)>> -> %{registers | h: value}
+      <<0b101::size(3)>> -> %{registers | l: value}
+      <<0b111::size(3)>> -> %{registers | a: value}
+      _ -> registers
+    end
   end
 end
