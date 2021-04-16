d4f856e5981b82bbe887ca4d9445e133e35b9272
Bill Peregoy <bill@peregoy.org>
2021-04-16 12:20:38 -0400

    Added crude login/logout on Elm side

--- assets/elm/src/Main.elm
+++ assets/elm/src/Main.elm
@@ -1,56 +1,86 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, button, div, h1, img)
+import Html exposing (Html, button, div, h1, img, text)
 import Html.Attributes exposing (src)
+import Html.Events exposing (onClick)
 import Http
 import Json.Decode exposing (Decoder, field, int, string)
+
 
 
 ---- MODEL ----
 
 
-type alias Model =
-    {pc: Maybe Int}
+type Model
+    = LoggedOut
+    | LoggedIn State
+
+
+type alias State =
+    { pc : Maybe Int }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {pc = Nothing}
-     , Http.get
-          { url = "http://localhost:4000/api/state"
-               , expect = Http.expectJson GotState decoder
-                    } )
+    ( LoggedOut, Cmd.none )
+
+
+
+-- ( LoggedIn { pc = Nothing }
+--, Http.get
+--   { url = "http://localhost:4000/api/state"
+--  , expect = Http.expectJson GotState decoder
+-- }
+-- )
 
 
 decoder : Decoder Int
-decoder = field "pc" int
+decoder =
+    field "pc" int
+
+
 
 ---- UPDATE ----
 
 
 type Msg
-    = NoOp
-      | GotState (Result Http.Error Int)
+    = Login
+    | Logout
+    | GotState (Result Http.Error Int)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    NoOp ->
-      ( model, Cmd.none )
-    GotState (Ok value) ->
-      ({model | pc = Just value}, Cmd.none)
-    GotState (Err (Http.BadUrl _)) ->
-      (model , Cmd.none)
-    GotState (Err Http.Timeout) ->
-      (model  , Cmd.none)
-    GotState (Err Http.NetworkError) ->
-      (model  , Cmd.none)
-    GotState (Err (Http.BadStatus _)) ->
-      (model  , Cmd.none)
-    GotState (Err (Http.BadBody _)) ->
-      (model  , Cmd.none)
+    case msg of
+        Login ->
+            ( LoggedIn { pc = Nothing }
+            , Http.get
+                { url = "http://localhost:4000/api/state"
+                , expect = Http.expectJson GotState decoder
+                }
+            )
+
+        Logout ->
+            ( LoggedOut, Cmd.none )
+
+        GotState (Ok value) ->
+            ( LoggedIn { pc = Just value }, Cmd.none )
+
+        GotState (Err (Http.BadUrl _)) ->
+            ( model, Cmd.none )
+
+        GotState (Err Http.Timeout) ->
+            ( model, Cmd.none )
+
+        GotState (Err Http.NetworkError) ->
+            ( model, Cmd.none )
+
+        GotState (Err (Http.BadStatus _)) ->
+            ( model, Cmd.none )
+
+        GotState (Err (Http.BadBody _)) ->
+            ( model, Cmd.none )
 
 
 
@@ -59,23 +89,36 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [
-         h1 [] [ text "TRS80 Emulator" ]
-         , div [] [
-           div [] [text "PC"]
-           , div [] [ text (formatPc model.pc)]
-           , button [] [text "Fetch"]
-           ]
-        ]
+    case model of
+        LoggedOut ->
+            div [] [ button [ onClick Login ] [ text "Login" ] ]
 
-formatPc : (Maybe Int) -> String
-formatPc val =
-  case val of
-    Nothing ->
-      "Not initialized"
-    Just num ->
-      String.fromInt num
+        LoggedIn _ ->
+            div []
+                [ h1 [] [ text "TRS80 Emulator" ]
+                , div []
+                    [ div [] [ text "PC" ]
+                    , div [] [ text (formatPc model) ]
+                    , button [ onClick Logout ] [ text "Logout" ]
+                    ]
+                ]
+
+
+formatPc : Model -> String
+formatPc model =
+    case model of
+        LoggedOut ->
+            "Not Logged In"
+
+        LoggedIn { pc } ->
+            case pc of
+                Nothing ->
+                    "Not Initialized"
+
+                Just num ->
+                    String.fromInt num
+
+
 
 ---- PROGRAM ----
 
